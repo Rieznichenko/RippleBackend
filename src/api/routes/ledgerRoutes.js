7e622e7750d3ce90e2e5fcfcead59ac8fe16b575
HungryCrawler019 <hopemalik0512@gmail.com>
2024-02-05 04:46:15 -0500

    fix:route

--- src/api/routes/ledgerRoutes.js
+++ src/api/routes/ledgerRoutes.js
@@ -10,6 +10,6 @@ router.get('/history', ledgerController.getLedgerHistory);
 router.get('/:id/transactions', ledgerController.getLedgerTransactions);
 
 //Route to get most significant transactions in last 24 hours
-router.get('/:id/transactions', ledgerController.getLedgerTransactions);
+router.get('/:id/transactions', ledgerController.getSiginificantTransactions);
 
 module.exports = router;
